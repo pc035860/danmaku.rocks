9b4371aa1d588b11b22ff9a4c37d67484b8c9bb2
pc035860 <pc035860@gmail.com>
2017-05-01 01:11:01 +0800

    optimize request caching

--- app/twitchApi.js
+++ app/twitchApi.js
@@ -8,18 +8,26 @@ const twitchApi = (url, clientId = twitchClientId) => {
 };
 
 const channelCache = {};
-
+const channelRequesting = {};
 export const getChannel = (channel) => {
   if (channelCache[channel]) {
     return $.when(channelCache[channel]);
   }
 
-  return twitchApi(`https://api.twitch.tv/v5/users?login=${channel}`)
+  if (channelRequesting[channel]) {
+    // returns a promise
+    return channelRequesting[channel];
+  }
+
+  const promise = twitchApi(`https://api.twitch.tv/v5/users?login=${channel}`)
   .then((res) => {
     const channel = res.users[0];
     channelCache[channel] = channel;
+    delete channelRequesting[channel];
     return channel;
   });
+  channelRequesting[channel] = promise;
+  return promise;
 };
 
 export const getChannelId = (channel) => {
